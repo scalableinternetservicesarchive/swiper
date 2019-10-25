4212c76bbff54b44c385493029a570902a21e2a1
kojiboji <kojiboji@gmail.com>
2019-10-25 02:16:57 -0700

    change fields in each listing

--- swiper/app/models/listing.rb
+++ swiper/app/models/listing.rb
@@ -1,4 +1,20 @@
 class Listing < ApplicationRecord
     belongs_to :user
+    enum location: %i[
+                    b_plate 
+                    covel 
+                    de_neve 
+                    feast 
+
+                    b_cafe 
+                    nineteen_nineteen 
+                    rende  
+                    study 
+    ]
+    enum status: %i[
+        posted  
+        matched  
+        completed 
+    ]
 end
   
--- swiper/test/factories/listings.rb
+++ swiper/test/factories/listings.rb
@@ -1,7 +1,10 @@
 FactoryBot.define do
   factory :listing do
-    name { Faker::Hipster.sentence }
-    description { Faker::Hipster.paragraph }
+    # name { Faker::Hipster.sentence }
+    # description { Faker::Hipster.paragraph }
+    price {4.0 + Faker::Number.within(range: 0..6)*0.25}
+    location {Faker::Number.within(range: 0..7)}
+    amount {Faker::Number.within(range: 1..5)}
     association(:user)
   end
 end
