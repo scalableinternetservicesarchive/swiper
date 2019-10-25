896a821b5af784227f6d780b91b74ef68bbead88
kojiboji <kojiboji@gmail.com>
2019-10-25 11:14:08 -0700

    make the names of the listings pretty

--- swiper/app/models/listing.rb
+++ swiper/app/models/listing.rb
@@ -16,5 +16,20 @@ class Listing < ApplicationRecord
         matched  
         completed 
     ]
+
+    def pretty_location
+        names =  {
+            b_plate: "BPlate", 
+            covel: "Covel", 
+            de_neve: "DeNeve", 
+            feast: "Feast", 
+
+            b_cafe: "BCafe", 
+            nineteen_nineteen: "1919", 
+            rende: "Rende",  
+            study: "Study"
+        }
+        names[location.to_sym]
+    end
 end
   
