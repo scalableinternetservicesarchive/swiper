3fa8381700e2c3581520abb506841a72c23220b9
Aaron Yoo <atyoo99@gmail.com>
2019-10-29 13:19:44 -0700

    got a basic profile working that shows your listing

---
+++ swiper/app/controllers/profile_controller.rb
@@ -0,0 +1,9 @@
+class ProfileController < ApplicationController
+    before_action :require_login
+
+    def show
+        @user = User.find(params[:id])
+        redirect_to root_path unless @user == current_user
+        @listings = @user.listings
+    end
+end
