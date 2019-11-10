e3aefb1c130197283036011431855d2f1160a5e4
Ameya Thakur <redsupercar@gmail.com>
2019-11-10 09:51:32 -0800

    finish homepage (minus images), fix some styling

--- swiper/app/helpers/application_helper.rb
+++ swiper/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def lowest_price(location)
+        number_with_precision(Listing.where(location: location, buyer: nil).order(:price).first.price, precision: 2)
+    end
+
+    def filter_checked(location)
+        return 1 if :filter && :filter[location] && :filter[location] == '1'
+        0
+    end
 end
