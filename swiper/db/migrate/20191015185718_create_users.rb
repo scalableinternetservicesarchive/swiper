245583d9712be30698e9f427f85ceb085582c750
Aaron Yoo <atyoo99@gmail.com>
2019-11-23 16:18:15 -0800

    finished rating workflow

--- swiper/db/migrate/20191015185718_create_users.rb
+++ swiper/db/migrate/20191015185718_create_users.rb
@@ -6,6 +6,8 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :encrypted_password, limit: 128, null: false
       t.string :confirmation_token, limit: 128
       t.string :remember_token, limit: 128, null: false
+      t.float  :rating, null: false, default: 0.0
+      t.integer :rating_count, null: false, default: 0
     end
 
     add_index :users, :email
