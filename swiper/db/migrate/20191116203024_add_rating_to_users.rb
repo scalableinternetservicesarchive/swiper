a2868e9e1b6a8be22ca46375fde43370ac1231e3
Aaron Yoo <atyoo99@gmail.com>
2019-12-02 16:53:42 -0800

    Deleted Migration that caused Deployment Error

--- swiper/db/migrate/20191116203024_add_rating_to_users.rb
+++
@@ -1,6 +0,0 @@
-# class AddRatingToUsers < ActiveRecord::Migration[6.0]
-#   def change
-#     add_column :users, :rating, :float, :default => 0
-#     add_column :users, :rating_count, :integer, :default => 0
-#   end
-# end
